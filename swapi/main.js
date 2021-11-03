ee07cf58c4682783cc60ab7d5a4cda8907f33b93
Jeff-Derbes <derbes.jeff@gmail.com>
2021-11-03 16:13:54 -0500

    initial commit

---
+++ swapi/main.js
@@ -0,0 +1,30 @@
+
+const button = document.querySelector('button')
+
+button.addEventListener('click', () => {
+    axios.get('https://swapi.dev/api/planets/2/')
+        .then((res) => {
+            let residents = res.data.residents
+            for (people of residents) {
+                axios.get(people)
+                    .then(res => {
+                        let newh2 = document.createElement('h2')
+                        newh2.textContent = res.data.name
+                        document.querySelector('.names').append(newh2)
+                    })
+            }
+        })
+})
+
+// axios.get('https://swapi.dev/api/planets/2/')
+//     .then((res) => {
+//         let residents = res.data.residents
+//         for (people of residents) {
+//             axios.get(people)
+//                 .then(res => {
+//                     let newh2 = document.createElement('h2')
+//                     newh2.textContent = res.data.name
+//                     document.querySelector('body').append(newh2)
+//                 })
+//         }
+//     })
